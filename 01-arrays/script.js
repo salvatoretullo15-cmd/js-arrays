629a6b94b8df0b2828fadcf34c3524a6b877e061
salvatore tullo <salvatoretullo15@gmail.com>
2026-02-05 09:59:32 +0100

    rimosse //

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -12,24 +12,24 @@ const teachers = [
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 
 const fourthTeacher = teachers[3];
-//console.log(fourthTeacher)
+console.log(fourthTeacher)
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
 teachers[4]= "Patrick";
-//console.log(teachers);
+console.log(teachers);
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 
 const lastTeacher= teachers.pop();
-//console.log(lastTeacher , teachers);
+console.log(lastTeacher , teachers);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 
 const firstTeacher = teachers.shift();
-//console.log(firstTeacher , teachers);
+console.log(firstTeacher , teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push("Vanessa")
